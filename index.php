1c45ad73a2302bba2a2c2fb9360c293282fbb648
BrandonHernandez2708 <bhernandez11@miumg.edu.gt>
2025-09-05 18:00:40 -0600

    index inicial

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Restaurante Marea Roja </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/boxicons.min.css" rel="stylesheet">
+  <link href="css/glightbox.min.css" rel="stylesheet">
+  <link href="css/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+ 
+  <!-- CSS de Bootstrap Icons desde CDN -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+
+
+
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center fixed-top topbar-transparent topbar-scrolled">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-center justify-content-lg-start">
+      <i class="bi bi-phone d-flex align-items-center"><span>+502 30816909</span></i>
+      <i class="bi bi-clock ms-4 d-none d-lg-flex align-items-center"><span>Horario -Lunes a Domingo : 9:00 AM - 19:00 PM</span></i>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent header-scrolled">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <div class="logo me-auto">
+        <h1><a href="index.html">Marea Roja</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
+          <li><a class="nav-link scrollto" href="#about">Sobre Nosotros</a></li>
+          <li><a class="nav-link scrollto" href="#menu">Menu</a></li>
+          <li><a class="nav-link scrollto" href="#gallery">Galeria</a></li>
+
+          
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"><li data-bs-target="#heroCarousel" data-bs-slide-to="0" class="active" aria-current="true"></li><li data-bs-target="#heroCarousel" data-bs-slide-to="1" class=""></li><li data-bs-target="#heroCarousel" data-bs-slide-to="2" class=""></li></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item carousel-item-next carousel-item-start" style="background-image: url(image/slide-1.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown"></h2>
+                <p class="animate__animated animate__fadeInUp"></p>
+                <div>
+                  <a> </a>
+                  <a< a="">
+                </a<></div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background-image: url(image/slide-2.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Ceviche de Camarón </h2>
+                <p class="animate__animated animate__fadeInUp">Elaborado con los camarones mas frescos de primera calidad.</p>
+                <div>
+                  <a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto"> Menu</a>
+                  <a <="" a="">
+                </a></div><a <="" a="">
+              </a></div><a <="" a="">
+            </a></div><a <="" a="">
+          </a></div><a <="" a="">
+
+          <!-- Slide 3 -->
+          </a><div class="carousel-item active carousel-item-start" style="background-image: url(image/slide-3.jpg);"><a <="" a="">
+            </a><div class="carousel-container"><a <="" a="">
+              </a><div class="carousel-content"><a <="" a="">
+                <h2 class="animate__animated animate__fadeInDown">Mariscos, pastas, carnes y mas.</h2>
+                <p class="animate__animated animate__fadeInUp">Aquí tenemos para todos los gusto.</p>
+                </a><div><a <="" a="">
+                  </a><a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto">Menu</a>
+                  <a <="" a="">
+                </a></div><a <="" a="">
+              </a></div><a <="" a="">
+            </a></div><a <="" a="">
+          </a></div><a <="" a="">
+
+        </a></div><a <="" a="">
+
+        </a><a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row">
+
+          <div class="col-lg-5 align-items-stretch video-box" style="background-image: url(&quot;image/about.jpg&quot;);">
+            <a href="https://www.youtube.com/watch?v=da0g6FHeqX0" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+          </div>
+
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
+
+            <div class="content">
+              <h3>Somos el restaurante de mariscos <strong>mas grande de Guatemala</strong></h3>
+              <p>
+                Contamos con más 20 años de experiencia deleitando a guatemaltecos y extranjeros con los sabores más frescos y productos de la más alta calidad traídos directamente&nbsp;del&nbsp;mar.
+              </p>
+              <p class="fst-italic">
+                Buscanos en nuestras dos sucursales
+              </p>
+              <ul>
+                <li><i class="bi bi-check-all"></i> Marea Roja El Rancho km 82.5 Ruta al Atlántico.</li>
+                <li><i class="bi bi-check-all"></i> Marea Roja Santa Cruz en el km Rio Hondo Zacapa .</li>
+
+              </ul>
+              <p>
+                Al llegar al lugar te recibirá una plaza temática con diferentes opciones de comida y como  principal atracción  Marea Roja un rancho con aproximadamente 20 metros de altura con un ambiente cálido y acogedor. La decoración marina te transportará a las profundidades del mar, creando una experiencia única. El personal amable y atento estará listo para atenderte y asegurarse de que tengas una experiencia gastronómica&nbsp;inolvidable.
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+    <!-- ======= Menu Section ======= -->
+    <section id="menu" class="menu">
+      <div class="container">
+
+        <div class="section-title">
+
+        </div>
+        <section id="gallery" class="gallery">
+          <div class="container-fluid">
+
+            <div class="section-title">
+              <h2>Nuestro <span> Menu</span></h2>
+              <p> </p>
+            </div>
+
+            <div class="row g-0">
+
+              <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                  <a href="image/gallery-1m.jpg" class="gallery-lightbox">
+                    <img src="image/gallery-1m.jpg" alt="" class="img-fluid">
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                  <a href="image/gallery-2m.jpg" class="gallery-lightbox">
+                    <img src="image/gallery-2m.jpg" alt="" class="img-fluid">
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                  <a href="image/gallery-3m.jpg" class="gallery-lightbox">
+                    <img src="image/gallery-3m.jpg" alt="" class="img-fluid">
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                  <a href="image/gallery-4m.jpg" class="gallery-lightbox">
+                    <img src="image/gallery-4m.jpg" alt="" class="img-fluid">
+                  </a>
+                </div>
+
+
+    </div></div></div></section><!-- End Menu Section -->
+
+
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container-fluid">
+
+        <div class="section-title">
+          <h2>Galeria de <span> nuestro Restaurante</span></h2>
+          <p> </p>
+        </div>
+
+        <div class="row g-0">
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-1.jpg" class="gallery-lightbox">
+                <img src="image/gallery-1.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-2.jpg" class="gallery-lightbox">
+                <img src="image/gallery-2.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-3.jpg" class="gallery-lightbox">
+                <img src="image/gallery-3.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-4.jpg" class="gallery-lightbox">
+                <img src="image/gallery-4.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-5.jpg" class="gallery-lightbox">
+                <img src="image/gallery-5.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-6.jpg" class="gallery-lightbox">
+                <img src="image/gallery-6.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-7.jpg" class="gallery-lightbox">
+                <img src="image/gallery-7.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-8.jpg" class="gallery-lightbox">
+                <img src="image/gallery-8.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-9.jpg" class="gallery-lightbox">
+                <img src="image/gallery-9.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-10.jpg" class="gallery-lightbox">
+                <img src="image/gallery-10.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-11.jpg" class="gallery-lightbox">
+                <img src="image/gallery-11.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="gallery-item">
+              <a href="image/gallery-12.jpg" class="gallery-lightbox">
+                <img src="image/gallery-12.jpg" alt="" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><span>Encuentranos en </span>Maps</h2>
+        </div>
+      </div>
+
+      <div class="map">
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4584.950854259969!2d-90.02006782475465!3d14.910956241676525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589fdbe0583cd07%3A0xb471e21319dbe1b1!2sMarea%20Roja%20El%20Rancho%20Oficial!5e0!3m2!1ses!2sgt!4v1696356838367!5m2!1ses!2sgt" frameborder="0" allowfullscreen=""></iframe>
+      </div>
+
+      <div class="container mt-5">
+
+        <div class="info-wrap">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 info">
+              <i class="bi bi-geo-alt"></i>
+              <h4>Ubicación:</h4>
+              <p>km 81.5 ruta al Atlántico<br>El Rancho, El Progreso</p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-clock"></i>
+              <h4>Horarios de atención:</h4>
+              <p>Lunes a Domingo:<br>9:00 AM - 17:00 PM</p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-envelope"></i>
+              <h4>Email:</h4>
+              <p>info@restaurantemarearoja.com<br></p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
+              <i class="bi bi-phone"></i>
+              <h4>Telefono:</h4>
+              <p>+502 3081-6909 14</p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </section><!-- End Contact Section -->
+
+  </section></div></section></main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Marea Roja </h3>
+      <p>En MAREA ROJA, nos enorgullecemos de servir los mariscos más frescos y deliciosos&nbsp;de&nbsp;Guatemala.</p>
+      <div class="social-links">
+
+        <a href="https://www.facebook.com/marearojaelrancho" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/marearojaoficial/" class="instagram"><i class="bi bi-instagram"></i></a>
+        
+        
+      </div>
+      <div class="copyright">
+        <!--poner copy aqui-->
+
+      </div>
+      <div class="credits">
+
+        <!--poner creditos aqui -->
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center active"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="javascript/bootstrap.bundle.min.js"></script>
+  <script src="javascript/glightbox.min.js"></script>
+  <script src="javascript/isotope.pkgd.min.js"></script>
+  <script src="javascript/swiper-bundle.min.js"></script>
+  <script src="javascript/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="javascript/main.js"></script>
+
+
+
+
+</body></html>
